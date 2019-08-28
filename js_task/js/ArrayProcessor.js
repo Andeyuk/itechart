bcce35790e38e975ca979625df0ea3d66c31ab2f
Yahor Andrayuk <Yahor.Andrayuk@itechart-group.com>
2019-08-28 16:38:58 +0300

    +listener for user input

--- js_task/js/ArrayProcessor.js
+++ js_task/js/ArrayProcessor.js
@@ -3,49 +3,58 @@
 let input = document.getElementById('work-space__input');
 let output = document.getElementById('work-space__output');
 
-let ArrayProcessor = {};
+input.addEventListener('change', function(){
+    let numbers = this.value.match(/-?\d+/g)
+    for(let i = 0; i < numbers.length; i++){
+        numbers[i] = parseInt(numbers[i]);
+    }
+    output.textContent = ArrayProcessor.subSum(numbers);
+});
 
-function subSum(arr) {
-    let curSum = 0;
-    let index = 0;
-    let i = 0;
-    
-    for (; i < arr.length; i++) {
-        if (curSum + arr[i] >= curSum) {
-            //   console.log('curSum + arr[i] >= curSum',curSum , arr[i]);
-            curSum = curSum + arr[i];
-            //   console.log(curSum)
-            index = i;
-        } else {
-            console.log(
-                curSum,
-                arr[i],
-                arr[i + 1],
-                curSum + arr[i] + arr[i + 1] >= curSum 
-                && curSum >= arr[i + 1] 
-                && index + 1 == i,
-                index + 1,
-                i
-            );
-            if (
-                curSum + arr[i] + arr[i + 1] >= curSum 
-                && curSum >= arr[i + 1] 
-                && index + 1 == i
-            ) {
+let ArrayProcessor = {
+    subSum(arr) {
+        let curSum = 0;
+        let index = 0;
+        let i = 0;
+        
+        for (; i < arr.length; i++) {
+            if (curSum + arr[i] >= curSum) {
+                //   console.log('curSum + arr[i] >= curSum',curSum , arr[i]);
+                curSum = curSum + arr[i];
+                //   console.log(curSum)
+                index = i;
+            } else {
                 console.log(
-                    'curSum + arr[i] + arr[i+1]',
                     curSum,
                     arr[i],
-                    arr[i + 1]
+                    arr[i + 1],
+                    curSum + arr[i] + arr[i + 1] >= curSum 
+                    && curSum >= arr[i + 1] 
+                    && index + 1 == i,
+                    index + 1,
+                    i
                 );
-                curSum = curSum + arr[i] + arr[i + 1];
-                index = i + 1;
-            } else {
-                curSum = arr[i + 1] >= curSum ? arr[i + 1] : curSum;
+                if (
+                    curSum + arr[i] + arr[i + 1] >= curSum 
+                    && curSum >= arr[i + 1] 
+                    && index + 1 == i
+                ) {
+                    console.log(
+                        'curSum + arr[i] + arr[i+1]',
+                        curSum,
+                        arr[i],
+                        arr[i + 1]
+                    );
+                    curSum = curSum + arr[i] + arr[i + 1];
+                    index = i + 1;
+                } else {
+                    curSum = arr[i + 1] >= curSum ? arr[i + 1] : curSum;
+                }
+                i++;
             }
-            i++;
         }
+        console.log(i-1);
+        return curSum;
     }
-    console.log(i-1);
-    return curSum;
-}
+};
+
