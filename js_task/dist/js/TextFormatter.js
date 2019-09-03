c2eb8f35eb9a3dcb5e29febd083ebe9acb27f097
Yahor Andrayuk <Yahor.Andrayuk@itechart-group.com>
2019-09-03 10:50:05 +0300

    finished TextFormatter

--- js_task/dist/js/TextFormatter.js
+++ js_task/dist/js/TextFormatter.js
@@ -1,71 +1,65 @@
-/*
-options {
-    maxWidth,
-    maxRows,
-    whiteSpace
-}
-whiteSpace = symbol word line nowrap
-*/
+function textFormatter(string, maxLength, maxRows, whiteSpace) {
+    if (
+        (!maxLength && (whiteSpace != 3 || whiteSpace != 'line')) ||
+        (whiteSpace == 4 || whiteSpace == 'nowrap')
+    )
+        return string;
 
-function textFormatter(string, maxLength, maxRows, whiteSpace){
+    if (!maxRows) maxRows = Infinity;
 
-    if (!maxLength) return string;
-    if (!maxRows) maxRows = Math.round(string.length/(maxLength-1));
-
-    let rows = []
+    let rows = [];
     let shift = 0;
-    for (let i = 0; i < maxRows; i++){
+    //ignore max length coz it wouild be wrapped by symbol or word not line
+    if (whiteSpace == 3 || whiteSpace == 'line')
+        return string.match(/[^\.]+[\.]?[\ ]?/g);
+
+    for (let i = 0; i < maxRows; i++) {
         let tmp = string.slice(
-                i*(maxLength - 1) + shift,
-                (i+1)*(maxLength - 1) + shift
-            );
-        
-        if((i+1)*(maxLength - 1) + shift >= string.length ){
+            i * (maxLength - 1) + shift,
+            (i + 1) * (maxLength - 1) + shift
+        );
+
+        let rowEndIndex = (i + 1) * (maxLength - 1) + shift;
+
+        if (rowEndIndex >= string.length) {
             rows.push(tmp);
             break;
         }
 
-        let lastLetter = string[(i+1)*(maxLength - 1) + shift];
+        let lastLetter = string[rowEndIndex];
+        let preLastLetter = string[rowEndIndex - 1];
         
-           
-            let preLastLetter = string[(i+1)*(maxLength - 1) - 1 + shift];
-            //console.log(tmp ,preLastLetter+lastLetter, lastLetter.match(/[a-zA-Z]/) && preLastLetter.match(/[a-zA-Z]/),preLastLetter.match(/\s/) && lastLetter.match(/[a-zA-Z]/) || preLastLetter.match(/\s/) && lastLetter.match(/[a-zA-Z]/));
+        if (lastLetter.match(/[a-zA-Z]/) && preLastLetter.match(/[a-zA-Z]/)) {
+            if (whiteSpace == 'symbol' || whiteSpace == 1) tmp += '-';
 
-            if ( lastLetter.match(/[a-zA-Z]/) && preLastLetter.match(/[a-zA-Z]/)){
-                if (whiteSpace == 'symbol' || whiteSpace == 1)
-                    tmp += '-';
+            if (whiteSpace == 'word' || whiteSpace == 2) {
+                let j = 1;
 
-                if (whiteSpace == 'word' || whiteSpace == 2){
-                    console.log('word')
-                    let j=0;
-                    for (; lastLetter != ' '; j++){
-                        if( j > (i+1)*(maxLength - 1) + shift){
-                            break; //to do
-                        }
-                        lastLetter = string[(i+1)*(maxLength - 1) + shift - j];
-                    }
-                    console.log('tmp',tmp.slice(tmp.length - j, j - 1), tmp.length - j, j - 1, tmp.length, j);
-                    tmp = tmp.slice(tmp.length - j, j - 1);
+                //find space from the row end
+                for (; lastLetter != ' ' && j < tmp.length; j++) {
+                    lastLetter = tmp[tmp.length - j];
                 }
+
+                //cutting up to space character
+                console.log(tmp, lastLetter, j);
+                j -= 2;
+                tmp = tmp.slice(0, -j);
+                //shift index not to loose data
+                shift -= j;
+            }
+        } else {
+            if ( preLastLetter.match(/\s/) && lastLetter.match(/[a-zA-Z]/)
+            || preLastLetter.match(/[a-zA-Z]/) && lastLetter.match(/\s/)){
+                tmp += '';
             }
             else {
-                if ((preLastLetter.match(/\s/) && lastLetter.match(/[a-zA-Z]/)))
-                    tmp += ' ';
-                else {
-                    if (preLastLetter.match(/[a-zA-Z]/) && lastLetter.match(/\s/)){
-                        tmp += ' ';
-                        shift++;
-                    }
-                    else {
-                        tmp += string[(i+1)*(maxLength - 1) + 1];
-                        shift++;
-                    }
-                }
+                tmp += lastLetter;
+                shift++;
             }
+        }
 
         rows.push(tmp);
     }
 
-    console.log(rows);
     return rows;
-    }
+}
