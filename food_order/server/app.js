64d9fbbd1dabc1249e2767deb5dd4bda63755aab
Yahor Andrayuk <Yahor.Andrayuk@itechart-group.com>
2019-10-08 15:51:54 +0300

    add routes

--- food_order/server/app.js
+++ food_order/server/app.js
@@ -26,13 +26,18 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-const routes = require('./routes/index');
+//const routes = require('./routes/index');
 const auth = require('./routes/auth')
 const dishes = require('./routes/dishes');
+const profile = require('./routes/profile');
+const operations = require('./routes/operations');
 
-app.use('/', routes);
+
+//app.use('/', routes);
 app.use('/auth', auth);
 app.use('/dishes', dishes);
+app.use('/profile', profile);
+app.use('/operations', operations);
 
 
 module.exports = app;
