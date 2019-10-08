ea56993c0b84708d76a3fba0f03e3c9977100082
Yahor Andrayuk <Yahor.Andrayuk@itechart-group.com>
2019-10-08 15:54:02 +0300

    add checkout api

---
+++ food_order/src/API/CartAPI.js
@@ -0,0 +1,20 @@
+import {AuthAPI} from './AuthAPI';
+
+export const CartAPI = {
+    checkout(purchases){
+        return AuthAPI.fetch('/operations/checkout',{
+            method:'Post',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                purchases
+            })
+        })
+        .then(res => {
+            if(res.status !== 200) throw new Error(res.status + ':' + res.statusText)
+            return res.json()
+        })
+        .catch(err=>err)
+    }
+}
