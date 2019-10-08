61371276ff23118b502130a0273ada2d149a266f
Yahor Andrayuk <Yahor.Andrayuk@itechart-group.com>
2019-10-08 15:49:28 +0300

    add popular route

--- food_order/server/routes/dishes.js
+++ food_order/server/routes/dishes.js
@@ -1,26 +1,88 @@
 const express = require('express');
 const router = express.Router();
 const passport = require('passport');
+const sequelize = require('../db/init');
+const Sequelize = require('sequelize');
 
 const Dish = require('../db/models/dish');
+const PopularDishes = require('../db/models/poplarDishes');
+const Order = require('../db/models/order');
+const OrderLine = require('../db/models/orderLine');
 
-
-router.get('/generate', (req, res) => {
-    Dish
-        .bulkCreate(
-            [ {name:"lorem4", price: 400, coockingTime: 700, description:'none'},
-            {name:"lorem1", price: 400, coockingTime: 700, description:'none'},
-            {name:"lorem2", price: 400, coockingTime: 700, description:'none'},])
-        .then(r=>res.send('OK'));
-  });
 
   
 router.get('/', (req, res) => {
+    const {start = 0, limit = 30} = req.query;
 
-    Dish.findAll().then(dishes=>
+    Dish.findAll({
+        attributes: {
+            include: [[sequelize.fn('SUM', sequelize.col('OrderLine.amount')), 'isCookingAmount']], 
+            exclude: ['createdAt', 'updatedAt']
+        },
+        include: [{
+            attributes: [],
+            model: OrderLine,
+            include:{
+                attributes: [],
+                model: Order,
+                where:{
+                    isActive: true,
+                },
+                //required:true,
+            }
+        },],
+        offset: start,
+        limit: limit,
+        group:[
+            'Dish.id',
+        ],
+        raw: true,
+
+    }).then(dishes=>
         res.send(dishes)
     );
-
+ 
 })
+
+router.get('/popular', (req, res) => {
+    const {start = 0, limit = 30} = req.query;
+
+    PopularDishes.findAll({
+        attributes: [
+            [Sequelize.col('Dish.id'), 'id'],
+            [Sequelize.col('Dish.name'), 'name'],
+            [Sequelize.col('Dish.cookingTime'), 'cookingTime'],
+            [Sequelize.col('Dish.price'), 'price'],
+            [Sequelize.col('Dish.description'), 'description'],
+            [Sequelize.fn('SUM', sequelize.col('Dish->OrderLine.amount')), 'isCookingAmount'],
+        ],
+        include:{
+            attributes: [],
+            model: Dish,
+            include: [{
+                attributes: [],
+                model: OrderLine,
+                include:{
+                    attributes: [],
+                    model: Order,
+                    where:{
+                        isActive: true,
+                    },
+                    //required:true,
+                }
+            }]
+        },
+        offset: start,
+        limit: limit,
+        group:[
+            'PopularDishes.id',
+            'Dish.id'
+        ],
+        raw: true,
+
+    }).then(dishes=>
+        res.json(dishes)
+    );
+});
   
   module.exports = router;
